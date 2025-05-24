7415407df4763ac7c5ad11b4653d7f19b3a446f9
yuki-kobayashi <ready-steady-go.reno@way.ocn.ne.jp>
2025-05-24 13:21:47 +0900

    カスタムユーザーモデルの取得の記述が誤っていたため修正

--- create_admin.py
+++ create_admin.py
@@ -1,13 +1,16 @@
 import os
 import django
 
+# 環境変数でDjangoの設定ファイルを指定
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+# Djangoを初期化
 django.setup()
 
-from django.contrib.auth.models import User
+# カスタムユーザーモデルの取得
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
+
+# 存在しない場合だけスーパーユーザーを作成
 if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser("admin", "admin@ne.jp", "n5wdf71qag")
-    print("Superuser created.")
-else:
-    print("Superuser already exists.")
+    User.objects.create_superuser("admin", "admin@example.com", "password")
