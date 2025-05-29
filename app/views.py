c077e3d8eb2ea9704512130976c178f5d97c00d2
yuki-kobayashi <ready-steady-go.reno@way.ocn.ne.jp>
2025-05-29 10:37:49 +0900

    修正戻し

--- app/views.py
+++ app/views.py
@@ -7,7 +7,6 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.mail import send_mail
 from django.http import JsonResponse
 from accounts.models import CustomUser
-from .serializers import PaymentSerializer
 from .models import Item, OrderItem, Order, Payment, CarouselTitle, Favorite
 
 
