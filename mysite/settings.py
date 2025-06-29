22e4793ca7b6976fe62bd4e2458d61ea54cfc2fe
yuki-kobayashi <ready-steady-go.reno@way.ocn.ne.jp>
2025-06-29 12:40:42 +0900

    管理画面のcssが解除されているため修正

--- mysite/settings.py
+++ mysite/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "app", "static")]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "app", "static")]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
